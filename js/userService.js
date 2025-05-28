633e0fb6cc1ddde3624ed58b26d6a26f56b89b2c
PIPEFRANCO <afrancogutierrez581@gmail.com>
2025-05-27 20:25:31 -0500

    Agregar usuario

--- js/userService.js
+++ js/userService.js
@@ -1,8 +1,8 @@
 function users(page) {
     document.getElementById('cardHeader').innerHTML = '<h5>Listado de usuarios</h5>'
-    const REQRES_ENDPOINT = "https://reqres.in/api/users?page="+page;
+    const REQRES_ENDPOINT = "https://reqres.in/api/users" + page;
     fetch(REQRES_ENDPOINT, {
-      method: "GET",
+      method: "POST",
       headers: {
         "Content-type": "application/json",
         "x-api-key": "reqres-free-v1",
@@ -21,6 +21,7 @@ function users(page) {
         console.log("resultado ", result);
         if (result.status === 200) {
           let listusers = `
+          <button type="button" class="btn btn-outline-success" onclick="createUser()">Ver</button>
               <table class="table">
     <thead>
       <tr>
@@ -40,7 +41,9 @@ function users(page) {
                   <td>${element.first_name}</td>
                   <td>${element.last_name}</td>
                   <td> <img src="${element.avatar}" class="img-thumbnail" alt="Avatar del usuario"></td>
-                  <td><button type="button" class="btn btn-outline-info" onclick="getUser('${element.id}')">Ver</button></td>
+                  <td>
+                    <button type="button" class="btn btn-outline-info" onclick="getUser('${element.id}')">Ver</button>
+                  </td>
             </tr>
             `
           });
@@ -132,3 +135,98 @@ function users(page) {
         }
     })
   }
+
+function createUser(){
+  const modalUser =`
+  <div class="modal fade" id="modalUser" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Crear Usuario</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="card">
+              <div class="card-body">
+                  <form id="formCreateUser">
+                    <div class="row">
+                      <div class="col">
+                        <input type="text" class="form-control" id="first_name" placeholder="First name" aria-label="First name" required>
+                      </div>
+                      <div class="col">
+                        <input type="text" class="form-control" id="last_name" placeholder="Last name" aria-label="Last name" required>
+                      </div>
+                    </div>
+                    <div class="row mt-3">
+                      <div class="col">
+                        <input type="email" class="form-control" id="email" placeholder="Email" aria-label="First name" required>
+                      </div>
+                      <div class="col">
+                        <input type="password" class="form-control" id="password" placeholder="Password" aria-label="Last name" required>
+                      </div>
+                    </div>
+                    <div class="row mt-3">
+                      <div class="col">
+                        <button type="button" class="btn btn-success" onclick="saveUser()">"Close"></button>
+                      </div>
+                    </div>
+                  </form>
+              </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  `
+  document.getElementById('viewModal').innerHTML = modalUser
+  const modal = new bootstrap.Modal(document.getElementById('modalUser'))
+  modal.show()
+}
+
+  function saveUser(){
+    const form = document.getElementById('formCreateUser')
+    if(form.checkValidaty()){
+      const first_name = document.getElementById('first_name').value
+      const last_name = document.getElementById('last_name').value
+      const email = document.getElementById('email').value
+      const password = document.getElementById('password').value
+      const user = {first_name, last_name, email, password}
+
+      const REQRES_ENDPOINT = "https://reqres.in/api/users";
+    fetch(REQRES_ENDPOINT, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+        "x-api-key": "reqres-free-v1",
+      },
+      body: JSON.stringify(user)
+    })
+    .then((result) =>{
+        return result.json().then(
+            data =>{
+                return {
+                    status: result.status,
+                    body: data
+                }
+            }
+        )
+    })
+    .then((response) =>{
+      if(response.status === 201){
+        document.getElementById('info').innerHTML = 
+        '<h3>Se guardo el usuario</h3>'
+      } else {
+        document.getElementById('info').innerHTML = 
+        '<h3>Error al guardar el usuario</h3>'
+      }
+      const modalId = document.getElementById('modalUser')
+      const modal = new bootstrap.Modal.getInstance(modal)
+      modal.hide()
+    })
+  } else {
+    form.reportValidity()
+  }
+}
